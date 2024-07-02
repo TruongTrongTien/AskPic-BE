ca7dbc4b3c2a0e0dcff87011c519e804fa40f32a
TruongTrongTien <trongtientruong2003@gmail.com>
2024-07-02 11:40:37 +0700

    add CORSMiddleware

--- main.py
+++ main.py
@@ -6,12 +6,22 @@ from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.exceptions import RequestValidationError
 from pydantic import ValidationError
+from fastapi.middleware.cors import CORSMiddleware
 
 from apis.controllers.controller import askpic_router
 
 app = FastAPI()
 app.title = "AskPic API Service"
-app.version = "0.0.1"
+app.version = "0.0.1"     
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins, change this in production to specific domains
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Create a GET method that responds with HTML code
 @app.get('/', tags = ['home'])
