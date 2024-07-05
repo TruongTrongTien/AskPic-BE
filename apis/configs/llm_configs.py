b651c0460c8e98ae468f44c925af759610877130
TruongTrongTien <trongtientruong2003@gmail.com>
2024-07-05 09:46:47 +0700

    update llm configs

--- apis/configs/llm_configs.py
+++ apis/configs/llm_configs.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import JsonOutputParser, StrOutputParser
 
 # Import API key
 load_dotenv()
@@ -11,4 +11,6 @@ GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
 
 llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", temperature=0, api_key=GOOGLE_API_KEY, request_timeout=120)
 
-parser = JsonOutputParser()
+json_parser = JsonOutputParser()
+
+str_parser = StrOutputParser()
