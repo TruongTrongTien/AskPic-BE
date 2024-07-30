d573aa24a60d84d2ef7369282b9839feba4642b8
TruongTrongTien <trongtientruong2003@gmail.com>
2024-07-30 21:40:08 +0700

    create and add generative models settings

---
+++ apis/utils/const.py
@@ -0,0 +1,26 @@
+GENERATIVE_MODELS_SAFETY_SETTINGS = [
+    {
+        "category": "HARM_CATEGORY_DANGEROUS",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HARASSMENT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_HATE_SPEECH",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+        "threshold": "BLOCK_NONE",
+    },
+    {
+        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+        "threshold": "BLOCK_NONE",
+    },
+]
+
+GENERATIVE_MODELS_GENERATION_CONFIGS = {
+    "temperature": 0,
+}
